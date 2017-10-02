e5b68c26d6e875c6becad88662386fca4a2e722c
Laith <info@laithazzam.com>
2017-10-01 22:38:27 -0700

    intial push

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp        = require( 'gulp' );
+var runSequence = require( 'run-sequence' );
+var requireDir  = require( 'require-dir' );
+
+
+
+// Require all tasks.
+requireDir( './gulp/tasks', { recurse: true } );
+
+
+// Commonly used tasks defined here.
+gulp.task( 'default', function(  )
+{
+	runSequence(
+		'clean',
+		[
+			'jade', 
+			'sass',
+			'scripts',
+			'images',
+			'videos',
+			'favicon'
+		],
+		'watch',
+		'connect'
+	);
+} );
+
+
+
+// Build task.
+gulp.task( 'build', function(  )
+{
+	runSequence(
+		'clean',
+		[
+			'build-images',
+			'build-scripts',
+			'build-css'
+		],
+		'build-html',
+		'connect'
+	);
+} );
